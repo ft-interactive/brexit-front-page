260b03948556a27ddd0703327df3f5cc6043ba9c
Viktor Charypar <charypar@gmail.com>
2015-09-18 16:57:36 +0100

    Don't nobody want backticks

--- server/graphql/backend-adapters/popular-api.js
+++ server/graphql/backend-adapters/popular-api.js
@@ -6,7 +6,7 @@ class PopularAPI {
 	topics(ttl = 50) {
 		const url = 'https://ft-next-popular-api.herokuapp.com/topics?apiKey=' + process.env.POPULAR_API_KEY;
 
-		return this.cache.cached(`popular-api.topics`, ttl, () => {
+		return this.cache.cached('popular-api.topics', ttl, () => {
 			return fetch(url)
 			.then((response) => response.json());
 		});
