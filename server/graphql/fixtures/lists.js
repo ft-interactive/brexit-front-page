8218da127d083c4d73a1372589fadd9e54014d80
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-10-06 11:16:30 +0100

    updating tests' fixtures

--- server/graphql/fixtures/lists.js
+++ server/graphql/fixtures/lists.js
@@ -6,15 +6,15 @@ export default {
 		publishedDate: '2015-06-24T09:44:00.000Z',
 		items: [
 			{
-				id: 'http://api.ft.com/thing/3600a424-199c-11e5-a130-2e7db721f996',
+				id: 'http://api.ft.com/things/3600a424-199c-11e5-a130-2e7db721f996',
 				apiUrl: 'http://api.ft.com/content/3600a424-199c-11e5-a130-2e7db721f996'
 			},
 			{
-				id: 'http://api.ft.com/thing/dc12711c-19ab-11e5-a130-2e7db721f996',
+				id: 'http://api.ft.com/things/dc12711c-19ab-11e5-a130-2e7db721f996',
 				apiUrl: 'http://api.ft.com/content/dc12711c-19ab-11e5-a130-2e7db721f996'
 			},
 			{
-				id: 'http://api.ft.com/thing/265ceb78-15ae-11e5-8e6a-00144feabdc0',
+				id: 'http://api.ft.com/things/265ceb78-15ae-11e5-8e6a-00144feabdc0',
 				apiUrl: 'http://api.ft.com/content/265ceb78-15ae-11e5-8e6a-00144feabdc0'
 			}
 		]
