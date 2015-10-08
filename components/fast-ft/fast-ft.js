3812b64fdea1a4cd355adf4ef13d4f2ec9ee5d53
Viktor Charypar <charypar@gmail.com>
2015-10-08 10:07:47 +0100

    No inline styles.

--- components/fast-ft/fast-ft.js
+++ components/fast-ft/fast-ft.js
@@ -5,7 +5,7 @@ class FastFt extends Component {
 	render() {
 		const articleEls = this.props.articles.slice(0, 6).map(article => <li key={article.id}><FastFtItem article={article} /></li>);
 		return (
-			<div style={{display: 'flex'}}>
+			<div className="fast-ft-wrapper">
 				<div className="fast-ft">
 					<h2><span className="fast-ft__logo fast-ft__logo--fast">fast</span><span className="fast-ft__logo fast-ft__logo--ft">FT</span></h2>
 					<ol className="fast-ft__items">
