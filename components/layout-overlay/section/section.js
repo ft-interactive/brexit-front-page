ea9e291397b138c0e5598c294c1bf73c1c794e32
Viktor Charypar <charypar@gmail.com>
2015-10-12 12:10:09 +0100

    Keep laout consistent and update widths when moving cards between columns

--- components/layout-overlay/section/section.js
+++ components/layout-overlay/section/section.js
@@ -2,6 +2,23 @@ import React, {Component} from 'react';
 
 import CardEditor from '../card-editor/card-editor';
 
+// makes sure layout description is consistent
+const enforceConstraints = (cards) => {
+	return cards.map((card, idx) => {
+		if(idx < 1) return card;
+
+		// ensure monotonic columns
+		card.column = Math.max(+card.column, +cards[idx - 1].column);
+		card.column = Math.min(+card.column, +cards[idx - 1].column + 1);
+
+		// ensure constant widths in columns
+		if(+card.column === +cards[idx - 1].column)
+			card.width = +cards[idx - 1].width
+
+		return card;
+	});
+}
+
 export default class Section extends Component {
 	constructor(props) {
 		super(props);
@@ -18,7 +35,9 @@ export default class Section extends Component {
 		return (newCard) => {
 			const newCards = this.props.cards.slice();
 			const widthDiff = +newCards[idx].width - newCard.width;
+			const columnDiff = +newCard.column - newCards[idx].column;
 
+			// bail if we're making a card too narrow
 			if(newCard.width < 2) return;
 
 			// on width change, update a neihgboring column width
@@ -26,15 +45,20 @@ export default class Section extends Component {
 				const column = +newCards[idx].column;
 				const cardToChange = newCards.find((card) => +card.column > column) || newCards.find((card) => +card.column === column - 1)
 
+				// bail if we're making a card too narrow
 				if((+cardToChange.width + widthDiff) < 2) return;
 
 				cardToChange.width = +cardToChange.width + widthDiff;
-				// bail if we're making a card too narrow
+			}
+
+			// on column change, take a width of the target column
+			if(columnDiff !== 0) {
+				newCard.width = newCards[idx + columnDiff].width;
 			}
 
 			newCards[idx] = newCard;
 
-			this.props.onCardsChange(newCards);
+			this.props.onCardsChange(enforceConstraints(newCards));
 		};
 	}
 
