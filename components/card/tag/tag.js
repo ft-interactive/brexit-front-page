c0b21dfb206313bb1523f7df7298196808bddf03
andygout <andy.gout@ft.com>
2015-10-20 11:22:29 +0100

    Change: Article card topics from <a> to <p>

--- components/card/tag/tag.js
+++ components/card/tag/tag.js
@@ -4,9 +4,9 @@ export default class Tag extends Component {
 	render() {
 		const tag = this.props.tag;
 		return (
-			<a className={'card__tag card__tag--' + tag.taxonomy + ' card__tag--' + this.props.size} href={tag.href} data-trackable="tag">
+			<p className={'card__tag card__tag--' + tag.taxonomy + ' card__tag--' + this.props.size}>
 				{tag.name}
-			</a>
+			</p>
 		);
 	}
 }
