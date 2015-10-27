19b5cf794a1c716646043e03db7dcfb67b99868a
Viktor Charypar <charypar@gmail.com>
2015-10-27 17:52:32 +0000

    Simplify computing the colspans

--- components/layout/engine.js
+++ components/layout/engine.js
@@ -28,12 +28,13 @@ const maximumCards = (layouts) => {
 // This may assign one card into multiple columns, rendering it twice
 const colspans = (layouts, storyIndex) => {
 	const columns = {}; // column -> colspan
-	const definedLayouts = Object.keys(layouts);
+	const definedLayouts = ['default'].concat(layoutNames.filter(it => !!layouts[it]));
+	const cardNotInDefault = layouts.default && !layouts.default[storyIndex];
 
 	// first pass: assign card into columns as requested
 
 	definedLayouts.forEach((layout) => {
-		if(!layouts[layout][storyIndex]) return;
+		if(!layouts[layout] || !layouts[layout][storyIndex]) return;
 
 		const card = layouts[layout][storyIndex];
 		const column = card.column || 0;
@@ -43,49 +44,44 @@ const colspans = (layouts, storyIndex) => {
 		columns[column][layout] = width;
 	});
 
-	// second pass: add defaults and 'hide's
+	// second pass: move lone defaults a column up to avoid placing always hidden cards
 
-	// add 'default' to layouts if it's missing
-	if(definedLayouts[0] !== 'default') definedLayouts.unshift('default');
-
-	// existing columns = columns card is visible in,
-	// first one should be default 12 unless explicitly hidden
 	const requestedColumns = Object.keys(columns);
-
 	if(requestedColumns.length > 1) {
-		const firstColumnLayouts = Object.keys(columns[requestedColumns[0]]);
+		const firstColumn = requestedColumns[0];
+		const secondColumn = requestedColumns[1];
+		const firstColumnLayouts = Object.keys(columns[firstColumn]);
+
 		if(firstColumnLayouts.length < 2 && firstColumnLayouts[0] === 'default') {
-			columns[requestedColumns[1]].default = columns[requestedColumns[0]].default;
-			delete columns[requestedColumns[0]];
+			columns[secondColumn].default = columns[firstColumn].default;
+
+			delete columns[firstColumn];
 		}
 	}
 
+	// third pass: 'hide' cards already handled in other layouts and set defaults
+
 	const existingColumns = Object.keys(columns);
-
-	// if there is an explicit default layout and it doesn't have the card
-	// hide the card in default
-	if(layouts.default && !layouts.default[storyIndex])
-		columns[existingColumns[0]].default = 'hide';
-
 	existingColumns.forEach((column, i) => {
-		columns[column].default = columns[column].default || (i === 0 ? 12 : 'hide');
-		columns[column].default = columns[column].default === 'hide' ? columns[column].default : 12;
+		// default layout for the column is 12 for the first column,
+		// hide for others or cards skipped in default
+		columns[column].default = ((i !== 0 || cardNotInDefault) ? 'hide' : 12);
 
-		definedLayouts.forEach((layout) => {
+		// for layouts apart from default
+		definedLayouts.slice(1).forEach((layout) => {
 			if(columns[column][layout]) return;
 
-			if(!layouts[layout][storyIndex])
+			const cardNotDefined = !layouts[layout][storyIndex];
+			// yes, this is a very nested loop but they are quite small constants -
+			// the some callback runs 50 times total for the complex layout example
+			const coveredInOtherColumn = existingColumns.some((c) => (c !== column && columns[c][layout]));
+
+			if(cardNotDefined || coveredInOtherColumn)
 				columns[column][layout] = 'hide';
-
-			// hide in all the layouts defined in any of the other columns
-			existingColumns.forEach((c) => {
-				if(column === c) return;
-
-				if(columns[c][layout] || !layouts[layout][storyIndex])
-					columns[column][layout] = 'hide';
-			});
-		})
+		});
 	});
+
+	// finally, done.
 
 	return columns;
 };
@@ -131,11 +127,13 @@ const buildColumns = (layouts, articles) => {
 	const maxCards = maximumCards(layouts);
 	const columns = [];
 
+	// place each card to its requested column or columns
 	for(let storyIndex = 0; storyIndex < maxCards; storyIndex++) {
 		const spans = colspans(layouts, storyIndex);
 		const props = cardProps(layouts, storyIndex);
 		const article = articles[storyIndex];
 
+		// put the card into each of the columns we need it in
 		for(const column in spans) {
 			if(spans.hasOwnProperty(column)) {
 				columns[column] = columns[column] || {cards: []};
@@ -148,6 +146,8 @@ const buildColumns = (layouts, articles) => {
 		};
 	}
 
+	// derive colspans by taking non-hide values from each layout of every card
+	// (they will be the same)
 	columns.forEach((column, cidx) => {
 		const colspan = column.cards.reduce((span, card) => {
 			Object.keys(card.colspan).forEach((layout) => {
