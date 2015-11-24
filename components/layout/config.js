ab6ebc2e68201a110b306dface142dd2d9de2b44
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-11-24 14:03:32 +0000

    layouts for other components

--- components/layout/config.js
+++ components/layout/config.js
@@ -78,37 +78,44 @@ export default [
 		style: 'opinion',
 		cards: {
 			default: [
-				{ size: 'large', standFirst: true, image: true },
-				{ size: 'medium', standFirst: true },
-				{ size: 'medium', standFirst: true },
+				{ size: 'medium', standFirst: true, image: true, landscape: true },
 				{ size: 'small' },
-				// { ad: true },
+				{ size: 'small', image: true, landscape: true },
+				{ size: 'small', image: true, landscape: true },
 				{ size: 'small' },
-				{ size: 'small' },
-				{ size: 'small' }
+				{ size: 'tiny' },
+				{ size: 'tiny', image: true, landscape: true },
+				{ size: 'tiny' }
 			],
 			S: [
-				{ size: 'large', standFirst: true, image: true },
-				{ size: 'medium', standFirst: true },
-				{ size: 'medium', standFirst: true },
+				{ size: 'medium', standFirst: true, image: true, landscape: true },
 				{ size: 'small' },
-				// { ad: true },
+				{ size: 'small', image: true, landscape: true },
+				{ size: 'small', image: true, landscape: true },
 				{ size: 'small' },
-				{ size: 'small' },
-				{ size: 'small' }
+				{ size: 'tiny' },
+				{ size: 'tiny', image: true, landscape: true },
+				{ size: 'tiny' }
 			],
 			M: [
-				{ column: 0, width: 4, size: 'large', standFirst: true, image: true },
-				{ column: 1, width: 3, size: 'medium' },
-				{ column: 1, width: 3, size: 'medium', image: true },
+				{ column: 0, width: 3, size: 'medium', standFirst: true, image: true },
+				{ column: 1, width: 3, size: 'small' },
+				{ column: 1, width: 3, size: 'small', image: true },
 				{ column: 2, width: 3, size: 'small', image: true },
 				{ column: 2, width: 3, size: 'small' },
+				{ column: 3, width: 3, size: 'tiny' },
+				{ column: 3, width: 3, size: 'tiny', image: true },
+				{ column: 3, width: 3, size: 'tiny' }
+			],
+			L: [
+				{ column: 0, width: 3, size: 'large', standFirst: true, image: true },
+				{ column: 1, width: 3, size: 'medium' },
+				{ column: 1, width: 3, size: 'small', image: true },
+				{ column: 2, width: 3, size: 'small', image: true },
 				{ column: 2, width: 3, size: 'small' },
-				// { column: 3, width: 4, ad: true },
-				{ column: 3, width: 2, size: 'tiny' },
-				{ column: 3, width: 2, size: 'tiny' },
-				{ column: 3, width: 2, size: 'tiny', image: true },
-				{ column: 3, width: 2, size: 'tiny' }
+				{ column: 3, width: 3, size: 'tiny' },
+				{ column: 3, width: 3, size: 'tiny', image: true },
+				{ column: 3, width: 3, size: 'tiny' }
 			]
 		},
 		size: {
@@ -179,32 +186,42 @@ export default [
 		style: 'editors-picks',
 		cards: {
 			default: [
-				{ size: 'large', standFirst: true, image: true },
-				{ size: 'medium', standFirst: true },
-				{ size: 'medium', standFirst: true },
+				{ size: 'medium', standFirst: true, image: true, landscape: true },
 				{ size: 'small' },
+				{ size: 'small', image: true, landscape: true },
+				{ size: 'small', image: true, landscape: true },
 				{ size: 'small' },
-				{ size: 'small' },
-				{ size: 'small' },
+				{ size: 'tiny' },
+				{ size: 'tiny' },
 				{ size: 'tiny' },
 				{ size: 'tiny' }
 			],
 			S: [
-				{ size: 'large', standFirst: true, image: true },
-				{ size: 'medium', standFirst: true },
-				{ size: 'medium', standFirst: true },
+				{ size: 'medium', standFirst: true, image: true, landscape: true },
 				{ size: 'small' },
+				{ size: 'small', image: true, landscape: true },
+				{ size: 'small', image: true, landscape: true },
 				{ size: 'small' },
-				{ size: 'small' },
-				{ size: 'small' },
+				{ size: 'tiny' },
+				{ size: 'tiny' },
 				{ size: 'tiny' },
 				{ size: 'tiny' }
 			],
 			M: [
-				{ column: 0, width: 3, size: 'large', image: true, standFirst: true },
-				{ column: 1, width: 3, size: 'medium', standFirst: true },
+				{ column: 0, width: 3, size: 'medium', standFirst: true, image: true },
+				{ column: 1, width: 3, size: 'small' },
+				{ column: 1, width: 3, size: 'small', image: true },
+				{ column: 2, width: 3, size: 'small', image: true },
+				{ column: 2, width: 3, size: 'small' },
+				{ column: 3, width: 3, size: 'tiny' },
+				{ column: 3, width: 3, size: 'tiny' },
+				{ column: 3, width: 3, size: 'tiny' }
+			],
+			L: [
+				{ column: 0, width: 3, size: 'large', standFirst: true, image: true },
 				{ column: 1, width: 3, size: 'medium' },
-				{ column: 2, width: 3, size: 'medium', image: true },
+				{ column: 1, width: 3, size: 'small', image: true },
+				{ column: 2, width: 3, size: 'small', image: true },
 				{ column: 2, width: 3, size: 'small' },
 				{ column: 3, width: 3, size: 'tiny' },
 				{ column: 3, width: 3, size: 'tiny' },
