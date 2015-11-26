bc104ced3e0e6bbbd0679a22730205f7b87f2d11
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-11-26 12:52:49 +0000

    use o-tabs for header tab

--- components/section/section.js
+++ components/section/section.js
@@ -4,20 +4,41 @@ import SectionContent from './section-content/section-content';
 
 import colspan from '../../client/utils/colspan';
 
+const classify = classes => classes
+	.filter(className => className)
+	.join(' ');
+
 export default class Section extends Component {
 	render () {
 		const cols = this.props.cols;
+		const sectionContentClasses = classify([
+			'section__column',
+			'section__column--content',
+			this.props.isTab ? 'o-tabs__tabpanel' : ''
+		]);
+		const sectionAsideClasses = classify([
+			'section__column',
+			'section__column--sidebar',
+			this.props.sidebarComponent && this.props.sidebarComponent.isTab ? 'o-tabs__tabpanel' : ''
+		]);
 		return (
-			<section className={'section o-grid-container o-grid-container--compact section--' + this.props.style} data-trackable={this.props.id}>
+			<section
+				className={'section o-grid-container o-grid-container--compact section--' + this.props.style}
+				data-trackable={this.props.id}>
 				<div className="o-grid-row">
 					{
 						cols.meta ?
-							<div data-o-grid-colspan={colspan(cols.meta)} className="section__column section__column--meta">
+							<div
+								data-o-grid-colspan={colspan(cols.meta)}
+								className="section__column section__column--meta">
 								<SectionMeta title={this.props.title} date={this.props.date} />
 							</div>
 							: null
 					}
-					<div data-o-grid-colspan={colspan(cols.content)} className="section__column section__column--content">
+					<div
+						id={this.props.id + '-section-content'}
+						data-o-grid-colspan={colspan(cols.content)}
+						className={sectionContentClasses}>
 						<SectionContent
 							style={this.props.style}
 							columns={this.props.columns}
@@ -27,7 +48,11 @@ export default class Section extends Component {
 					</div>
 					{
 						cols.sidebar ?
-							<aside data-o-grid-colspan={colspan(cols.sidebar)} className="section__column section__column--sidebar" data-trackable={this.props.sidebarComponent.id}>
+							<aside
+								id={this.props.id + '-section-aside'}
+								data-o-grid-colspan={colspan(cols.sidebar)}
+								className={sectionAsideClasses}
+								data-trackable={this.props.sidebarComponent.id}>
 								<this.props.sidebarComponent.component articles={this.props.sidebarContent.items} />
 								<div className="sidebar__ad-container"></div>
 							</aside>
