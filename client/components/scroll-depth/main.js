b741ac3139ff1aaaf0c8fdb420140fbf0b28e946
Arjun Gadhia <arjungadhia@gmail.com>
2015-12-03 14:26:10 +0000

    Add the domPath of the component to scroll depth tracking

--- client/components/scroll-depth/main.js
+++ client/components/scroll-depth/main.js
@@ -1,16 +1,17 @@
 const fireTracking = require('../../utils/fire-tracking');
 const throttle = require('../../utils/throttle');
+const getDomPath = require('n-instrumentation/src/utils/getDomPath');
 
 const toArray = nodeList => Array.prototype.slice.call(nodeList);
 
-const track = componentPos => fireTracking('oTracking.event', { category: 'page', action: 'scrolldepth', componentPos: componentPos });
+const track = (componentEl, componentPos) => fireTracking('oTracking.event', { category: 'page', action: 'scrolldepth', componentPos: componentPos, domPath: getDomPath(componentEl, []) });
 
 const scrollHandlerFactory = components => {
 	const componentsViewed = [];
 	return () => {
 		components.forEach((component, index) => {
 			if (component.getBoundingClientRect().top < window.innerHeight && componentsViewed.indexOf(component) === -1) {
-				track(index + 1);
+				track(component, index + 1);
 				componentsViewed.push(component);
 			}
 		});
