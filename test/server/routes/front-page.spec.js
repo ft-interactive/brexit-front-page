1063fb9eb3a07cfc11491bebfc8d628d140106bc
Darren Hurley <ironsidevsquincy@googlemail.com>
2015-12-10 12:06:57 +0000

    use nock

--- test/server/routes/front-page.spec.js
+++ test/server/routes/front-page.spec.js
@@ -1,4 +1,5 @@
 import request from 'supertest';
+
 import { app, listen } from '../../../server/app';
 
 describe('Front Page', () => {
@@ -8,13 +9,13 @@ describe('Front Page', () => {
     it('should be able to set edition to uk', done => {
         request(app)
             .get(`/uk?editionPref=uk`)
-            .expect('set-cookie', /^next-edition=uk; Max-Age=31536000; Domain=ft.com;/, done)
+            .expect('set-cookie', /\bnext-edition=uk; Max-Age=31536000; Domain=ft.com;/, done)
     });
 
     it('should be able to set edition to international', done => {
         request(app)
             .get(`/uk?editionPref=international`)
-            .expect('set-cookie', /^next-edition=international; Max-Age=31536000; Domain=ft.com;/, done)
+            .expect('set-cookie', /\bnext-edition=international; Max-Age=31536000; Domain=ft.com;/, done)
     });
 
 });
