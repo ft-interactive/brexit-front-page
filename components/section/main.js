a348b908c4c921bf3caf31639b07735ecf1ee03b
Arjun Gadhia <arjungadhia@gmail.com>
2015-12-11 14:09:04 +0000

    Make layout tool work again

--- components/section/main.js
+++ components/section/main.js
@@ -4,34 +4,31 @@ import ReactDOM from 'react-dom';
 import Section from './section';
 import initialLayout from '../layout/config';
 
-let element;
-let content;
-let layout;
 let showMostPopularByIndustry;
 
+const layoutForSection = (layout, sectionId) => layout.find((section) => (section.id === sectionId));
 
 function init (el, mostPopularByIndustry) {
 	if (!el) return;
-	const sectionId = el.id;
-	element = el;
-	layout = initialLayout.find((section) => (section.id === sectionId));
-	content = JSON.parse(el.getAttribute('data-section-content'));
 	showMostPopularByIndustry = mostPopularByIndustry;
-	render();
+	render(el, initialLayout);
 
 }
 
-function render () {
+function render (el, layout) {
 
+	let content = JSON.parse(el.getAttribute('data-section-content'));
+	let sectionLayout = layoutForSection(layout, el.id);
 	ReactDOM.render(<Section
-		{...layout}
+		{...sectionLayout}
 		content={content.body}
 		sidebarContent={content.sidebar}
 		showMostPopularByIndustry={showMostPopularByIndustry}
 		data-o-grid-colspan="12"/>,
-	element);
+	el);
 }
 
 export default {
-	init: init
+	init,
+	render
 }
