01da2cf0411598dc750263e3fea7bba856c1a9ce
Arjun Gadhia <arjungadhia@gmail.com>
2015-12-14 12:28:38 +0000

    Fix nojs to hide label as well

--- components/section/section-sources/section-sources.js
+++ components/section/section-sources/section-sources.js
@@ -4,9 +4,9 @@ class SectionSources extends Component {
 	render () {
 
 		return (
-			<div className="section-sources">
+			<div className="section-sources n-util-hide-no-js">
 				<label className="section-sources__label">{this.props.dynamicContent.description}</label>
-				<select className="section-sources__select n-util-hide-no-js" onChange={this.props.onChange} value={this.props.selectedSource}>
+				<select className="section-sources__select" onChange={this.props.onChange} value={this.props.selectedSource}>
 					{this.props.dynamicContent.sources.map(function(source) {
 							return <option key={source.title} value={source.uuid}>{source.title}</option>;
 					})}
