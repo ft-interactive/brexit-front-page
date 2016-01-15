eecaaa2b626189eff5a881fd5e46102fa6081149
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-01-15 16:57:47 +0000

    adding webpack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const BowerWebpackPlugin = require('bower-webpack-plugin');
+
+module.exports = {
+	entry: [
+		'./client/main.js'
+	],
+	output: {
+		path: './public',
+		filename: 'main.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				loader: 'babel',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			}
+		]
+	},
+	plugins: [
+		new BowerWebpackPlugin({
+			includes: /\.js$/
+		})
+	]
+};
