781de037b59c7973b86b3d61a2e2f29bb9bc69c9
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-01-19 11:57:23 +0000

    styling and better naming

--- components/card/concept.js
+++ components/card/concept.js
@@ -7,22 +7,30 @@ class Concept extends Component {
 		const concept = this.props.item;
 		const articlesWithImage = concept.items.filter(article => article.primaryImage);
 		const image = articlesWithImage.length ?
-			<img className="topic-card__image" src={'//next-geebee.ft.com/image/v1/images/raw/' + articlesWithImage[0].primaryImage.rawSrc + '?source=next&fit=scale-down&width=400'} /> :
+			<img className="card__topic-image" src={'//next-geebee.ft.com/image/v1/images/raw/' + articlesWithImage[0].primaryImage.rawSrc + '?source=next&fit=scale-down&width=400'} /> :
 			null;
 		const articles = concept.items.slice(0, 3).map(article => (
-			<li className="topic-card__article" key={article.id}>
-				<a className="topic-card__article-link" href={'/content/' + article.id} data-trackable="article">{article.title}</a>
+			<li className="card__topic-article" key={article.id}>
+				<a className="card__topic-article-link" href={'/content/' + article.id} data-trackable="article">{article.title}</a>
 			</li>
 		));
 
 		return (
-			<section className="topic-card" data-trackable="topic">
-				<div className="topic-card__topic-container">
-					<h2 className="topic-card__title">{concept.name}</h2>
-					<Follow conceptId={concept.id} name={concept.name} taxonomy={concept.taxonomy} />
+			<section className="card card--topic" data-trackable="topic">
+				<div className="card__topic-container">
+					<div className="card__topic-container__text">
+						<h2 className="card__topic-title">{concept.name}</h2>
+						<Follow
+							conceptId={concept.id}
+							name={concept.name}
+							taxonomy={concept.taxonomy}
+							variant="inverse"
+							size="big"
+							classes="card__topic-follow" />
+					</div>
 					{image}
 				</div>
-				<ol className="topic-card__articles" data-trackable="articles">
+				<ol className="card__topic-articles" data-trackable="articles">
 					{articles}
 				</ol>
 			</section>
