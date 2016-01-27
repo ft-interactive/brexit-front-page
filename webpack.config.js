2aee2cce1b7a3ee1733f66bf8b64e3f2864609cd
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-01-27 23:47:22 +0000

    use post css and plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
+const autoprefixer = require('autoprefixer');
 const BowerWebpackPlugin = require('bower-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	context: path.join(__dirname, 'client'),
 	entry: {
-		'main.js': './main.js',
+		'main.js': ['babel-polyfill', './main.js'],
 		'main.css': './main.scss',
 		'ie8.css': './ie8.scss'
 	},
@@ -18,9 +19,13 @@ module.exports = {
 			{
 				test: /\.js$/,
 				loader: 'babel',
+				exclude: /node_modules/,
 				query: {
 					presets: ['react', 'es2015'],
-					plugins: ['add-module-exports']
+					plugins: [
+						'add-module-exports',
+						['transform-es2015-classes', { loose: true }]
+					]
 				}
 			},
 			// force fastclick to load CommonJS
@@ -30,9 +35,14 @@ module.exports = {
 			},
 			{
 				test: /\.scss$/,
-				loader: ExtractTextPlugin.extract('css!autoprefixer!sass?includePaths[]=' + (path.resolve(__dirname, './bower_components')))
+				loader: ExtractTextPlugin.extract(
+					`css!postcss-loader!sass?includePaths[]=${path.resolve(__dirname, './bower_components')}`
+				)
 			}
 		]
+	},
+	postcss: function () {
+		return [autoprefixer({ browsers: ['> 1%', 'last 2 versions', 'ie > 6', 'ff ESR', 'bb >= 7'] })];
 	},
 	plugins: [
 		new BowerWebpackPlugin({
