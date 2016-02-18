87c687a18147e071b37b85541f0995a864d96507
Arjun Gadhia <arjungadhia@gmail.com>
2016-02-18 16:47:05 +0000

    Dont minify (lost in merge conflict)

--- server/routes/front-page.js
+++ server/routes/front-page.js
@@ -54,7 +54,6 @@ export default (region) => {
 			preconnect: [
 				'https://next-markets-proxy.ft.com'
 			],
-			minifyHtml: res.locals.flags.frontPageMinifyHtml,
 			adsLayout: res.locals.flags.frontPageAdFormats ? (req.query.adsLayout || 'impression') : 'default'
 		};
 		if (res.locals.flags.frontPageHeaderMarketsData) {
