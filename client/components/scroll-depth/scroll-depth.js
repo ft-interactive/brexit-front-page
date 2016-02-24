30316015f84833e517ec60a73f2bfa533a533811
andygout <andy.gout@ft.com>
2016-02-24 12:35:51 +0000

    Amend: Scroll depth handler to track dataTrackable (section name) and componentPos only

--- client/components/scroll-depth/scroll-depth.js
+++ client/components/scroll-depth/scroll-depth.js
@@ -1,12 +1,10 @@
 import fireTracking from '../../utils/fire-tracking';
 import throttle from '../../utils/throttle';
-import getDomPath from 'n-instrumentation/src/utils/getDomPath';
 
 const toArray = nodeList => Array.prototype.slice.call(nodeList);
 
-const track = (componentEl, componentPos) => {
-	const domPathTokens = getDomPath(componentEl, []);
-	fireTracking('oTracking.event', { category: 'page', action: 'scrolldepth', componentPos, domPathTokens: domPathTokens, domPath: domPathTokens.join(' | ') });
+const track = (dataTrackable, componentPos) => {
+	fireTracking('oTracking.event', { category: 'page', action: 'scrolldepth', dataTrackable, componentPos });
 }
 
 const scrollHandlerFactory = () => {
@@ -14,10 +12,10 @@ const scrollHandlerFactory = () => {
 	return () => {
 		toArray(document.querySelectorAll('.js-track-scroll-event'))
 			.forEach((component, index) => {
-				const componentId = component.getAttribute('data-trackable');
-				if (component.getBoundingClientRect().top < window.innerHeight && componentsViewed.indexOf(componentId) === -1) {
-					track(component, index + 1);
-					componentsViewed.push(componentId);
+				const dataTrackable = component.getAttribute('data-trackable');
+				if (component.getBoundingClientRect().top < window.innerHeight && componentsViewed.indexOf(dataTrackable) === -1) {
+					track(dataTrackable, index + 1);
+					componentsViewed.push(dataTrackable);
 				}
 			});
 	};
