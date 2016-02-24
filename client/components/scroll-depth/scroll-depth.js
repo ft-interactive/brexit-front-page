ab5500251a0e151c9bf4aa5be52a1505a1489a32
andygout <andy.gout@ft.com>
2016-02-24 11:56:17 +0000

    Amend: Scroll-depth handler to track separate domPathTokens and domPath values

--- client/components/scroll-depth/scroll-depth.js
+++ client/components/scroll-depth/scroll-depth.js
@@ -4,13 +4,15 @@ import getDomPath from 'n-instrumentation/src/utils/getDomPath';
 
 const toArray = nodeList => Array.prototype.slice.call(nodeList);
 
-const track = (componentEl, componentPos) =>
-	fireTracking('oTracking.event', { category: 'page', action: 'scrolldepth', componentPos, domPath: getDomPath(componentEl, []) });
+const track = (componentEl, componentPos) => {
+	const domPathTokens = getDomPath(componentEl, []);
+	fireTracking('oTracking.event', { category: 'page', action: 'scrolldepth', componentPos, domPathTokens: domPathTokens, domPath: domPathTokens.join(' | ') });
+}
 
 const scrollHandlerFactory = () => {
 	const componentsViewed = [];
 	return () => {
-		toArray(document.querySelectorAll('.section'))
+		toArray(document.querySelectorAll('.js-track-scroll-event'))
 			.forEach((component, index) => {
 				const componentId = component.getAttribute('data-trackable');
 				if (component.getBoundingClientRect().top < window.innerHeight && componentsViewed.indexOf(componentId) === -1) {
