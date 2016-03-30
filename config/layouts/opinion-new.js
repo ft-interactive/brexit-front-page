90ec0ef317b32255c5a69c5ddd28bdc8fb9cd361
andygout <andy.gout@ft.com>
2016-03-30 14:47:33 +0100

    Amend: Opinion section image size for L layout

--- config/layouts/opinion-new.js
+++ config/layouts/opinion-new.js
@@ -15,7 +15,7 @@ export default [
 						size: 'large',
 						showStandfirst: true,
 						image: {
-							sizes: { default: 449, s: 659, m: 277, l: 314, xl: 322 }
+							sizes: { default: 449, s: 659, m: 277, l: 259, xl: 322 }
 						}
 					}
 				]
