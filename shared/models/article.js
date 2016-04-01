e79c8ac4c104a1cd882732afd4725037cdc472a3
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-04-01 12:29:57 +0100

    group by type

--- shared/models/article.js
+++ shared/models/article.js
@@ -5,15 +5,34 @@ const propertyEquals = (property, value, object) => object[property] === value;
 const getPrimaryTag = ({ primaryTheme, primarySection }) =>
 	(primaryTheme && !['organisations', 'regions', 'people'].includes(primaryTheme.taxonomy)) ? primaryTheme : primarySection;
 
+const getType = item => {
+	if (item.type === 'LiveBlog') {
+		return 'live-blog';
+	} else if (item.type === 'FastFt') {
+		return 'fast-ft';
+	} else if (item.tags && item.tags.some(isCommentTag)) {
+		return 'opinion';
+	} else if (item.isEditorsChoice) {
+		return 'editors-pick';
+	} else {
+		return 'article';
+	}
+};
+
 export default (item, opts) => {
+	const type = getType(item);
 	const article = {
-		type: 'article',
+		type,
 		id: item.id,
 		title: item.title,
 		published: item.published,
 		lastPublished: item.lastPublished
 	};
-	const primaryTag = getPrimaryTag({ primaryTheme: item.primaryTheme, primarySection: item.primarySection });
+	const primaryTag = getPrimaryTag({
+		primaryTheme: item.primaryTheme,
+		primarySection: item.primarySection
+	});
+
 	if (opts.image && item.primaryImage && item.primaryImage.rawSrc) {
 		article.image = Object.assign({}, opts.image, { url: item.primaryImage.rawSrc });
 	}
@@ -23,10 +42,10 @@ export default (item, opts) => {
 	if (opts.isPictureStory) {
 		article.isPictureStory = opts.isPictureStory;
 	}
-	if (!opts.hideTag) {
+	if (!opts.hideTag && !['opinion', 'live-blog'].includes(type)) {
 		article.tag = primaryTag;
 	}
-	if (item.tags && item.tags.some(isCommentTag)) {
+	if (item.tags && type === 'opinion') {
 		const brand = item.tags.find(propertyEquals.bind(null, 'taxonomy', 'brand'));
 		if (item.authors.length || brand) {
 			const author = item.authors[0];
@@ -34,23 +53,23 @@ export default (item, opts) => {
 				article.brand = {
 					title: author.name,
 					url: author.url,
-					headshot: author.headshot
+					headshot: author.headshot,
+					type: 'opinion'
 				};
 			} else {
 				article.brand = {
 					title: brand.name,
-					url: brand.url
+					url: brand.url,
+					type: 'opinion'
 				};
 			}
 		} else {
 			article.brand = {
 				title: 'Opinion',
-				url: '/stream/sectionsId/MTE2-U2VjdGlvbnM='
+				url: '/stream/sectionsId/MTE2-U2VjdGlvbnM=',
+				type: 'opinion'
 			};
 		}
-		// don't show tags on opinion cards
-		article.tag = null;
-		article.type = 'opinion';
 	}
 	if (opts.related && opts.related.show) {
 		article.related = Object.assign({
@@ -60,21 +79,23 @@ export default (item, opts) => {
 				.slice(0, 3)
 		}, opts.related);
 	}
-	if (item.type === 'LiveBlog') {
-		article.type = 'live-blog';
+	if (type === 'live-blog') {
 		article.liveBlog = {
 			status: item.status,
 			latestUpdate: item.updates[0]
 		};
-		// no tags/brands on live blogs
-		article.tag = null;
-		article.brand = null;
 	}
-	if (item.type === 'FastFt') {
+	if (type === 'fast-ft') {
 		article.hideTimestampState = true;
 	}
 	if (opts.isMain) {
 		article.isMain = true;
+	}
+	if (type === 'editors-pick') {
+		article.brand = {
+			title: 'Editor\'s Pick',
+			type: 'editors-pick'
+		};
 	}
 
 	return article;
