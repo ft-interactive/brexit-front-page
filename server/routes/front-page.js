6805fe1271c1f20b9f1215dcf1c1aa5f032e3418
Arjun Gadhia <arjungadhia@gmail.com>
2016-04-05 16:28:45 +0100

    Nicer names or flag to turn on new ad proposition

--- server/routes/front-page.js
+++ server/routes/front-page.js
@@ -8,8 +8,18 @@ const contentMissing = data => {
 	return !(data && data.top && data.topStory) || data.top.items.length < 1|| data.topStory.items.length < 1 ;
 };
 
-const getAdsLayout = (requestedLayout) => {
-	return requestedLayout || 'default';
+const getAdsLayout = (requestedLayout, flags) => {
+	//map some url params to existing ad layout names
+
+	if(flags.adsNewProposition && !requestedLayout) {
+		requestedLayout = 'new';
+	}
+	const mapping = {
+		'responsive': 'highimpact',
+		'non-responsive-1': 'billboards',
+		'non-responsive-2': 'superleaders'
+	};
+	return mapping[requestedLayout] || requestedLayout || 'default';
 }
 
 export default region => (req, res) => {
@@ -17,7 +27,7 @@ export default region => (req, res) => {
 
 	const data = {
 		frontPage: getData(frontPageData),
-		adsLayout: getAdsLayout(req.query.adsLayout)
+		adsLayout: getAdsLayout(req.query.adsLayout, res.locals.flags)
 	};
 
 	res.set({
