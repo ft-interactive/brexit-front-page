18b059e823ccf2cb6fa7b43ce48c0a62d8555ede
Rhys Evans <rhys.evans@ft.com>
2016-04-22 17:09:57 +0100

    passing un-headerified mock mockFrontPage flag to graphql

--- server/libs/graphql-poller.js
+++ server/libs/graphql-poller.js
@@ -24,15 +24,17 @@ let queryResults = {
 let readyPromise;
 
 const pollData = (query, variables, name, flags = {}) => {
-	const xFlagsHeader = Object.keys(flags).map(flag => `${flag}:${flags[flag] ? 'on' : 'off'}`).join(',');
 	const poller = new Poller({
 		url: `https://next-graphql-api.ft.com/data?apiKey=${process.env.GRAPHQL_API_KEY}`,
 		options: {
 			method: 'POST',
-			body: JSON.stringify({ query, variables }),
+			body: JSON.stringify({
+				query,
+				variables
+			}),
 			headers: {
 				'Content-Type': 'application/json',
-				'X-Flags': xFlagsHeader
+				'FT-Graphql-Mock-Data': flags.mockFrontPage
 			}
 		},
 		parseData: function (results) {
