756c3d58f6258aba79374a0d9138e86eb0502484
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-05-03 14:53:46 +0100

    latest n-image

--- shared/components/myft-promo/myft-promo.js
+++ shared/components/myft-promo/myft-promo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Image from 'n-image';
+import { Image } from '@financial-times/n-image';
 
 export default class extends Component {
 	render () {
@@ -12,8 +12,9 @@ export default class extends Component {
 				<div className="myft-promo">
 					<Image
 						url="https://next-geebee.ft.com/assets/myft-tour/myft-in-situ.png"
-						imgClass="myft-promo__image"
-						srcset={{ default: 150, m: 277 }} />
+						classes="myft-promo__image"
+						widths={[150, 185, 220]}
+						sizes={{ default: '150px', m: '217px', L: '199px', XL: '207px' }} />
 					<p className="myft-promo__description">You choose the topics, we deliver the news.</p>
 					<p className="myft-promo__link-wrapper">
 						<a
