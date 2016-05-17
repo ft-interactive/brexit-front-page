1933dec1d6bc6174ce90942b06df8e23f185c196
Paul Wilson <paul.wilson66@gmail.com>
2016-05-17 17:42:30 +0100

    Update pages.js (#806)

--- config/pages.js
+++ config/pages.js
@@ -2,29 +2,13 @@ import getSection from './sections';
 import sectionData from '../server/libs/section-data';
 
 const getPages = flags => {
-	if(flags.asymmetricalFrontPageLayout){
-		return {
-			'front-page': [
-				'headlines',
-				'top-stories',
-				'mid-page-advert-1',
-				'opinion',
-				'editors-picks',
-				'myft',
-				'mid-page-advert-2',
-				'most-popular',
-				'technology',
-				'markets',
-				'videos'
-			]
-		}
-	}
+
 	return {
 		'front-page': [
 			'headlines',
 			'top-stories',
-			'mid-page-advert-1',
 			'opinion',
+			'mid-page-advert-1',
 			'myft',
 			'editors-picks',
 			'mid-page-advert-2',
