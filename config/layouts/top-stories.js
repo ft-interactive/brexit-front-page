b239167f7accf26d9b821278fdde348d8c8573f6
Paul Wilson <paul.wilson66@gmail.com>
2016-05-17 12:57:27 +0100

    fix non-aymmetrics layout (#803)

--- config/layouts/top-stories.js
+++ config/layouts/top-stories.js
@@ -6,41 +6,91 @@ export default [
 		components: [
 			{
 				type: 'Column',
-				colspan: { default: 12, M: 3 },
+				colspan: { default: 6, L: 3 },
 				components: [
 					{
 						type: 'Content',
-						size: 'small'
+						size: 'medium',
+						image: createImageConfig({ default: 6, L: 3 }, { default: 'top', S: 'right', L: 'top' }, [150, 216, 322])
 					}
 				]
 			},
 			{
 				type: 'Column',
-				colspan: { default: 12, M: 3 },
+				colspan: { default: 6, L: 3 },
 				components: [
 					{
 						type: 'Content',
-						size: 'small'
+						size: 'medium',
+						image: createImageConfig({ default: 6, L: 3 }, { default: 'top', S: 'right', L: 'top' }, [150, 216, 322])
 					}
 				]
 			},
 			{
 				type: 'Column',
-				colspan: { default: 12, M: 3 },
+				colspan: { default: 12, L: 6 },
 				components: [
 					{
-						type: 'Content',
-						size: 'small'
-					}
-				]
-			},
-			{
-				type: 'Column',
-				colspan: { default: 12, M: 3 },
-				components: [
+						type: 'Row',
+						components: [
+							{
+								type: 'Column',
+								colspan: { default: 12, S: 6 },
+								components: [
+									{
+										type: 'Content',
+										size: 'medium',
+										standfirst: {
+											show: { default: true }
+										}
+									}
+								]
+							},
+							{
+								type: 'Column',
+								colspan: { default: 12, S: 6 },
+								components: [
+									{
+										type: 'Content',
+										size: 'medium',
+										standfirst: {
+											show: { default: true }
+										}
+									}
+								]
+							}
+						]
+					},
 					{
-						type: 'Content',
-						size: 'small'
+						type: 'Row',
+						components: [
+							{
+								type: 'Column',
+								colspan: { default: 12, S: 6 },
+								components: [
+									{
+										type: 'Content',
+										size: 'medium'
+									}
+								]
+							},
+							{
+								type: 'Column',
+								colspan: { default: 12, S: 6 },
+								components: [
+									{
+										type: 'Content',
+										size: 'medium',
+										image: createImageConfig(
+											{ default: 12, S: 6, L: 3 },
+											{ default: 'right' },
+											[161, 225],
+											{ show: { default: true, S: false, XL: true }}
+										)
+									}
+								]
+							}
+						]
 					}
 				]
 			}
@@ -75,7 +125,13 @@ export default [
 				components: [
 					{
 						type: 'Content',
-						size: 'small'
+						size: 'small',
+						image: createImageConfig(
+							{ default: 12, M: 3 },
+							{ default: 'right' },
+							[161, 225],
+							{ show: { default: true, S: false, XL: true }}
+						)
 					}
 				]
 			},
