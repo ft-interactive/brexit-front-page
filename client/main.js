c534fb2f33507574232ca93022bcb6ee1b0246c8
Darren Hurley <ironsidevsquincy@googlemail.com>
2016-05-20 16:12:42 +0100

    js spacing

--- client/main.js
+++ client/main.js
@@ -1,11 +1,11 @@
-import {configure, bootstrap} from 'n-ui';
+import { configure, bootstrap } from 'n-ui';
 import nVideo from 'n-video';
 import highlightDomPath from './components/highlight-dom-path/highlight-dom-path';
 import scrollDepth from './components/scroll-depth/scroll-depth';
 import marketsData from './components/markets-data/markets-data';
 import myft from './components/myft/myft';
 
-configure({preset: 'complete'});
+configure({ preset: 'complete' });
 
 bootstrap(({ flags, mainCss }) => {
 	scrollDepth(flags);
